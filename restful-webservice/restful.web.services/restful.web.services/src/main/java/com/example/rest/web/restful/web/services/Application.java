299b272b5155674261cfe62442039a9c788d18ec
Dhanasekar S <dhanasty@gmail.com>
2026-04-28 12:01:51 +0530

    rest versioning

--- restful-webservice/restful.web.services/restful.web.services/src/main/java/com/example/rest/web/restful/web/services/Application.java
+++ restful-webservice/restful.web.services/restful.web.services/src/main/java/com/example/rest/web/restful/web/services/Application.java
@@ -2,7 +2,11 @@ package com.example.rest.web.restful.web.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
+@ComponentScan(basePackages = {
+	    "com.example.rest.web.restful.web.services",
+	    "versioning"   // add your versioning package here
+	})
 @SpringBootApplication
 public class Application {
 
